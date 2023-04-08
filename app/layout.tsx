34a89ab102a5749fa8d2cac4021451fc62a1e7bf
Chris Pikul <chris@workmaple.com>
2023-04-08 17:44:13 +0300

    added roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,23 @@
-import type { ReactNode } from 'react';
-import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
 
 import BackgroundImage from '@components/server/Background';
 
-import '@styles/global.scss';
+import type { ReactNode } from 'react';
+import type { Metadata } from 'next';
 
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+  fallback: [ 'Arial', 'Helvetica', 'sans-serif' ],
+});
+
+import '@styles/global.scss';
 export default function RootLayout({ children }: { children: ReactNode }):ReactNode {
   return (
-    <html lang="en">
+    <html lang="en" className={ roboto.className }>
       <body>
         <BackgroundImage />
         {children}
